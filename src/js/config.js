76dcbf209d8e67a33b4a507e7c6c7051a836bcd2
soul_kk <2986744287@qq.com>
2025-08-12 10:02:03 +0800

    fix: 修改API_KEY

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
 export const TIMEOUT_SEC = 60;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '0f16330a-bf8c-4344-9751-b935bc51ceb0';
 export const MODAL_CLOSE_SEC = 2.5;
