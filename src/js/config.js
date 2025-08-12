32a7367c765ba2ad13ea50fe4f5547fc2422c12c
soul_kk <2986744287@qq.com>
2025-08-12 09:56:50 +0800

    fix： 延长请求超时时间

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
-export const TIMEOUT_SEC = 10;
+export const TIMEOUT_SEC = 60;
 export const RES_PER_PAGE = 10;
 export const KEY = '<YOUR_KEY>';
 export const MODAL_CLOSE_SEC = 2.5;
